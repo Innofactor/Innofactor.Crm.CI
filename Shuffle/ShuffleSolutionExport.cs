3f665faa5cf14bf6af04a6a46c0e7d5b4f4fdfea
Jonas Rapp <jonas.rapp@cinteros.se>
2017-05-10 11:31:30 +0200

    Extracted ShuffleSolution**port to partial classes

---
+++ Shuffle/ShuffleSolutionExport.cs
@@ -0,0 +1,162 @@
+﻿using Cinteros.Crm.Utils.Common;
+using Cinteros.Crm.Utils.Misc;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Cinteros.Crm.Utils.Shuffle
+{
+    public partial class Shuffler
+    {
+        private void ExportSolutionBlock(XmlNode xBlock)
+        {
+            log.StartSection("ExportSolutionBlock");
+
+            if (xBlock.Name != "SolutionBlock")
+            {
+                throw new ArgumentOutOfRangeException("Type", xBlock.Name, "Invalid Block type");
+            }
+            string name = CintXML.GetAttribute(xBlock, "Name");
+            log.Log("Block: {0}", name);
+            string path = CintXML.GetAttribute(xBlock, "Path");
+            string file = CintXML.GetAttribute(xBlock, "File");
+            if (string.IsNullOrWhiteSpace(path) && !string.IsNullOrWhiteSpace(definitionpath))
+            {
+                path = definitionpath;
+                path += path.EndsWith("\\") ? "" : "\\";
+            }
+            if (string.IsNullOrWhiteSpace(file))
+            {
+                file = name;
+            }
+            XmlNode xExport = CintXML.FindChild(xBlock, "Export");
+            if (xExport != null)
+            {
+                string type = CintXML.GetAttribute(xExport, "Type");
+                string setversion = CintXML.GetAttribute(xExport, "SetVersion");
+                bool publish = CintXML.GetBoolAttribute(xExport, "PublishBeforeExport", false);
+                string targetversion = CintXML.GetAttribute(xExport, "TargetVersion");
+
+                CintDynEntity cdSolution = GetAndVerifySolutionForExport(name);
+                var currentversion = new Version(cdSolution.Property("version", "1.0.0.0"));
+
+                SendLine("Solution: {0} {1}", name, currentversion);
+
+                if (!string.IsNullOrWhiteSpace(setversion))
+                {
+                    SetNewSolutionVersion(setversion, cdSolution, currentversion);
+                }
+
+                if (publish)
+                {
+                    SendLine("Publishing customizations");
+                    crmsvc.Execute(new PublishAllXmlRequest());
+                }
+
+                ExportSolutionRequest req = new ExportSolutionRequest()
+                {
+                    SolutionName = name
+                };
+#if Crm8
+                if (!string.IsNullOrWhiteSpace(targetversion))
+                {
+                    req.TargetVersion = targetversion;
+                }
+#endif
+                XmlNode xSettings = CintXML.FindChild(xExport, "Settings");
+                if (xSettings != null)
+                {
+                    req.ExportAutoNumberingSettings = CintXML.GetBoolAttribute(xSettings, "AutoNumbering", false);
+                    req.ExportCalendarSettings = CintXML.GetBoolAttribute(xSettings, "Calendar", false);
+                    req.ExportCustomizationSettings = CintXML.GetBoolAttribute(xSettings, "Customization", false);
+                    req.ExportEmailTrackingSettings = CintXML.GetBoolAttribute(xSettings, "EmailTracking", false);
+                    req.ExportGeneralSettings = CintXML.GetBoolAttribute(xSettings, "General", false);
+                    req.ExportMarketingSettings = CintXML.GetBoolAttribute(xSettings, "Marketing", false);
+                    req.ExportOutlookSynchronizationSettings = CintXML.GetBoolAttribute(xSettings, "OutlookSync", false);
+                    req.ExportRelationshipRoles = CintXML.GetBoolAttribute(xSettings, "RelationshipRoles", false);
+                    req.ExportIsvConfig = CintXML.GetBoolAttribute(xSettings, "IsvConfig", false);
+                }
+
+                if (type == "Managed" || type == "Both")
+                {
+                    string filename = path + file + "_managed.zip";
+                    SendLine("Exporting solution to: {0}", filename);
+                    req.Managed = true;
+                    ExportSolutionResponse exportSolutionResponse = (ExportSolutionResponse)crmsvc.Execute(req);
+                    byte[] exportXml = exportSolutionResponse.ExportSolutionFile;
+                    File.WriteAllBytes(filename, exportXml);
+                }
+                if (type == "Unmanaged" || type == "Both")
+                {
+                    string filename = path + file + ".zip";
+                    SendLine("Exporting solution to: {0}", filename);
+                    req.Managed = false;
+                    ExportSolutionResponse exportSolutionResponse = (ExportSolutionResponse)crmsvc.Execute(req);
+                    byte[] exportXml = exportSolutionResponse.ExportSolutionFile;
+                    File.WriteAllBytes(filename, exportXml);
+                }
+            }
+            log.EndSection();
+        }
+
+        private void SetNewSolutionVersion(string setversion, CintDynEntity cdSolution, Version currentversion)
+        {
+            Version newversion = new Version();
+            if (setversion.Equals("IncrementAll", StringComparison.OrdinalIgnoreCase))
+            {
+                newversion = new Version("1.0.0.0");
+                foreach (var existingversion in ExistingSolutionVersions.Values)
+                {
+                    if (existingversion > newversion)
+                    {
+                        newversion = existingversion;
+                    }
+                }
+                newversion = IncrementVersion(newversion);
+            }
+            else if (setversion.Equals("Increment", StringComparison.OrdinalIgnoreCase))
+            {
+                newversion = IncrementVersion(currentversion);
+            }
+            else if (setversion.Equals("Current", StringComparison.OrdinalIgnoreCase))
+            {
+                newversion = currentversion;
+            }
+            else
+            {
+                newversion = new Version(setversion);
+            }
+            if (!currentversion.Equals(newversion))
+            {
+                SendLine("Setting version: {0}", newversion);
+                CintDynEntity cdSolUpd = cdSolution.Clone(true);
+                cdSolUpd.AddProperty("version", newversion.ToString());
+                cdSolUpd.Save();
+            }
+        }
+
+        private CintDynEntity GetAndVerifySolutionForExport(string name)
+        {
+            CintDynEntityCollection cSolutions = CintDynEntity.RetrieveMultiple(crmsvc, "solution",
+                new string[] { "isvisible", "uniquename" },
+                new object[] { true, name },
+                new ColumnSet("solutionid", "friendlyname", "version", "ismanaged"), log);
+            if (cSolutions.Count == 0)
+            {
+                throw new ArgumentOutOfRangeException("SolutionUniqueName", name, "Cannot find solution");
+            }
+            if (cSolutions.Count > 1)
+            {
+                throw new ArgumentOutOfRangeException("SolutionUniqueName", name, "Found " + cSolutions.Count.ToString() + " matching solutions");
+            }
+            CintDynEntity cdSolution = cSolutions[0];
+            return cdSolution;
+        }
+    }
+}
