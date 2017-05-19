5fce6769970148bf20aefa73e40879c7261b33b0
Alexey Shytikov <alexey.shytikov@cinteros.se>
2017-05-19 16:05:59 +0200

    Adding logic to versionAssembly task

--- CI/Extension/ApplyVersionToAssemblies/ApplyVersionToAssemblies.ps1
+++ CI/Extension/ApplyVersionToAssemblies/ApplyVersionToAssemblies.ps1
@@ -13,69 +13,88 @@
 # Enable -Verbose option
 [CmdletBinding()]
 
+# Getting input from UI
+$versionType = Get-VstsInput -Name versionType -Require
+$versionFile = Get-VstsInput -Name versionFile
+
+# File with resulting version
+$versionResult = Join-Path $env:BUILD_SOURCESDIRECTORY "version.txt"
+
+$NewVersion = ""
+
+# Checking if version should be taken from file and if that file does exist 
+if ($versionType -eq "file" -and Test-Path $versionFile)
+{
+    $Base = Get-Content $versionFile
+    Write-Host "Base from file $VersionFile $Base"
+    $NewVersion = "$Base.$(Rev:.rrr)"
+}
+else
+{
 # Regular expression pattern to find the version in the build number 
 # and then apply it to the assemblies
-$VersionRegex = "\d+\.\d+\.\d+\.\d+"
+    $VersionRegex = "\d+\.\d+\.\d+\.\d+"
 
 # If this script is not running on a build server, remind user to 
 # set environment variables so that this script can be debugged
-if(-not ($Env:BUILD_SOURCESDIRECTORY -and $Env:BUILD_BUILDNUMBER))
-{
-    Write-Error "You must set the following environment variables"
-    Write-Error "to test this script interactively."
-    Write-Host '$Env:BUILD_SOURCESDIRECTORY - For example, enter something like:'
-    Write-Host '$Env:BUILD_SOURCESDIRECTORY = "C:\code\FabrikamTFVC\HelloWorld"'
-    Write-Host '$Env:BUILD_BUILDNUMBER - For example, enter something like:'
-    Write-Host '$Env:BUILD_BUILDNUMBER = "Build HelloWorld_0000.00.00.0"'
-    exit 1
-}
+    if(-not ($Env:BUILD_SOURCESDIRECTORY -and $Env:BUILD_BUILDNUMBER))
+    {
+        Write-Error "You must set the following environment variables"
+        Write-Error "to test this script interactively."
+        Write-Host '$Env:BUILD_SOURCESDIRECTORY - For example, enter something like:'
+        Write-Host '$Env:BUILD_SOURCESDIRECTORY = "C:\code\FabrikamTFVC\HelloWorld"'
+        Write-Host '$Env:BUILD_BUILDNUMBER - For example, enter something like:'
+        Write-Host '$Env:BUILD_BUILDNUMBER = "Build HelloWorld_0000.00.00.0"'
+        exit 1
+    }
 
 # Make sure path to source code directory is available
-if (-not $Env:BUILD_SOURCESDIRECTORY)
-{
-    Write-Error ("BUILD_SOURCESDIRECTORY environment variable is missing.")
-    exit 1
-}
-elseif (-not (Test-Path $Env:BUILD_SOURCESDIRECTORY))
-{
-    Write-Error "BUILD_SOURCESDIRECTORY does not exist: $Env:BUILD_SOURCESDIRECTORY"
-    exit 1
-}
-Write-Verbose "BUILD_SOURCESDIRECTORY: $Env:BUILD_SOURCESDIRECTORY"
+    if (-not $Env:BUILD_SOURCESDIRECTORY)
+    {
+        Write-Error ("BUILD_SOURCESDIRECTORY environment variable is missing.")
+        exit 1
+    }
+    elseif (-not (Test-Path $Env:BUILD_SOURCESDIRECTORY))
+    {
+        Write-Error "BUILD_SOURCESDIRECTORY does not exist: $Env:BUILD_SOURCESDIRECTORY"
+        exit 1
+    }
+    Write-Verbose "BUILD_SOURCESDIRECTORY: $Env:BUILD_SOURCESDIRECTORY"
 
 # Make sure there is a build number
-if (-not $Env:BUILD_BUILDNUMBER)
-{
-    Write-Error ("BUILD_BUILDNUMBER environment variable is missing.")
-    exit 1
-}
-Write-Host "BUILD_BUILDNUMBER: $Env:BUILD_BUILDNUMBER"
+    if (-not $Env:BUILD_BUILDNUMBER)
+    {
+        Write-Error ("BUILD_BUILDNUMBER environment variable is missing.")
+        exit 1
+    }
+    Write-Host "BUILD_BUILDNUMBER: $Env:BUILD_BUILDNUMBER"
 
 # Get and validate the version data
-$VersionData = [regex]::matches($Env:BUILD_BUILDNUMBER,$VersionRegex)
-switch($VersionData.Count)
-{
-   0        
-      { 
-         Write-Error "Could not find version number data in BUILD_BUILDNUMBER."
-         exit 1
-      }
-   1 {}
-   default 
-      { 
-         Write-Warning "Found more than instance of version data in BUILD_BUILDNUMBER." 
-         Write-Warning "Will assume first instance is version."
-      }
+    $VersionData = [regex]::matches($Env:BUILD_BUILDNUMBER,$VersionRegex)
+    switch($VersionData.Count)
+    {
+       0        
+          { 
+             Write-Error "Could not find version number data in BUILD_BUILDNUMBER."
+             exit 1
+          }
+       1 {}
+       default 
+          { 
+             Write-Warning "Found more than instance of version data in BUILD_BUILDNUMBER." 
+             Write-Warning "Will assume first instance is version."
+          }
+    }
+    $NewVersion = $VersionData[0]
 }
-$NewVersion = $VersionData[0]
+
 Write-Host "Version: $NewVersion"
 
 # Write version number to file, to be able to pick up in later scripts
 # If working folder is the same, it can be read using:
 # $version = Get-Content .\version.txt
-$versionPath = Join-Path $env:BUILD_SOURCESDIRECTORY "version.txt"
-Write-Host "Saving version in file $versionPath"
-Set-Content -Path $versionPath -Value $NewVersion -Force
+Write-Host "Saving version in file $versionResult"
+Set-Content -Path $versionResult -Value $NewVersion -Force
 
 # Apply the version to the assembly property files
 $files = gci $Env:BUILD_SOURCESDIRECTORY -recurse -include "*Properties*","My Project","*Version*" | 
