8629026ce0f02748df4cf2aa3620c3fbbb480428
Jonas Rapp <jonas.rapp@innofactor.com>
2018-05-03 11:15:32 +0200

    Shuffle DevUtils 9.0.1805.1

--- Shuffle/Properties/AssemblyInfo.cs
+++ Shuffle/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("9.0.1711.0")]
-[assembly: AssemblyFileVersion("9.0.1711.0")]
+[assembly: AssemblyVersion("9.0.1805.1")]
+[assembly: AssemblyFileVersion("9.0.1805.1")]
