1be5b93b4903bbd7acefca7840561bd0e8f2f22c
Alexey Shytikov <alexey.shytikov@cinteros.se>
2018-07-11 14:33:06 +0200

    Combining build file

--- CI/Extension/PackExtension.cmd
+++ CI/Extension/PackExtension.cmd
@@ -1,4 +1,46 @@
 @echo off
+
+rem ===============================================================================
+echo 
+echo 
+echo 
+echo Assembling...
+
+set dllpath=%~dp0..\bin\Release\*.dll
+set extpath=%~dp0
+
+echo Looking for: %dllpath%
+echo Copying to : %extpath%
+
+if EXIST "%extpath%ShuffleExport\ps_modules" (del "%extpath%ShuffleExport\ps_modules" /F /S /Q)
+if EXIST "%extpath%ShuffleImport\ps_modules" (del "%extpath%ShuffleImport\ps_modules" /F /S /Q)
+if EXIST "%extpath%UpdateAssembly\ps_modules" (del "%extpath%UpdateAssembly\ps_modules" /F /S /Q)
+if EXIST "%extpath%UpdateWebResources\ps_modules" (del "%extpath%UpdateWebResources\ps_modules" /F /S /Q)
+if EXIST "%extpath%WhoAmI\ps_modules" (del "%extpath%WhoAmI\ps_modules" /F /S /Q)
+
+xcopy "%dllpath%" "%extpath%ShuffleExport\ps_modules\DevUtils.CI\"
+xcopy "%dllpath%" "%extpath%ShuffleImport\ps_modules\DevUtils.CI\"
+xcopy "%dllpath%" "%extpath%UpdateAssembly\ps_modules\DevUtils.CI\"
+xcopy "%dllpath%" "%extpath%UpdateWebResources\ps_modules\DevUtils.CI\"
+xcopy "%dllpath%" "%extpath%WhoAmI\ps_modules\DevUtils.CI\"
+xcopy "%dllpath%.config" "%extpath%ShuffleExport\ps_modules\DevUtils.CI\"
+xcopy "%dllpath%.config" "%extpath%ShuffleImport\ps_modules\DevUtils.CI\"
+xcopy "%dllpath%.config" "%extpath%UpdateAssembly\ps_modules\DevUtils.CI\"
+xcopy "%dllpath%.config" "%extpath%UpdateWebResources\ps_modules\DevUtils.CI\"
+xcopy "%dllpath%.config" "%extpath%WhoAmI\ps_modules\DevUtils.CI\"
+
+xcopy "%extpath%VstsTaskSdk" "%extpath%ApplyVersionToAssemblies\ps_modules\VstsTaskSdk\" /S /Y
+xcopy "%extpath%VstsTaskSdk" "%extpath%MinifyJS\ps_modules\VstsTaskSdk\" /S /Y
+xcopy "%extpath%VstsTaskSdk" "%extpath%ShuffleExport\ps_modules\VstsTaskSdk\" /S /Y
+xcopy "%extpath%VstsTaskSdk" "%extpath%ShuffleImport\ps_modules\VstsTaskSdk\" /S /Y
+xcopy "%extpath%VstsTaskSdk" "%extpath%UpdateAssembly\ps_modules\VstsTaskSdk\" /S /Y
+xcopy "%extpath%VstsTaskSdk" "%extpath%UpdateWebResources\ps_modules\VstsTaskSdk\" /S /Y
+xcopy "%extpath%VstsTaskSdk" "%extpath%WhoAmI\ps_modules\VstsTaskSdk\" /S /Y
+
+rem ===============================================================================
+echo 
+echo 
+echo 
 echo Packing!
 
 set /P updaterev=Update revision? [Y/N]: 
