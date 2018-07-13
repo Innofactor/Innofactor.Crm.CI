9f4a5ed4ed38b631bc2207a4f77b3d3d1c6e29da
Alexey Shytikov <alexey.shytikov@cinteros.se>
2018-07-13 13:18:00 +0200

    Migrating pack script from `cmd` to `ps1`

---
+++ Extension/pack.ps1
@@ -0,0 +1,122 @@
+Write-Host
+Write-Host
+Write-Host
+
+Write-Host "Assembling extension..."
+
+$Input	= (Get-Item -Path ".\..\Cmdlets\").FullName
+$Temp	  = (Get-Item -Path ".\Implementation\").FullName
+$Output = (Get-Item -Path ".\VSIX\").FullName
+
+Write-Host
+
+Write-Host "Cmdlets will be taken from:       '$Input'"
+Write-Host "Extension will be assemblied in:  '$Temp'"
+Write-Host "Binary result will be stored in:  '$Output'"
+
+Write-Host
+
+$TaskFolders = @(
+	"ApplyVersionToAssemblies"
+	"MinifyJS"
+	"ObfuscateAssembly"
+	"ShuffleExport"
+	"ShuffleImport"
+	"UpdateAssembly"
+	"UpdateWebResources"
+	"WhoAmI"
+)
+
+$DestinationFolders = @();
+
+# Creating list of temporary folders needs to be deleted in advance
+foreach($Folder in $TaskFolders)
+{
+  $DestinationFolders += "$Temp$Folder\ps_modules"
+}
+
+# Deleting all found temporary folder
+foreach($Folder in $DestinationFolders)
+{
+  if(Test-Path -Path $Folder)
+  {
+    Remove-Item -Path $Folder -Recurse -Force
+  }
+}
+
+$CmdletFolders = @(
+	""  # Some tasks may not require cmdlet, and in this case the path is empty
+	""  # Some tasks may not require cmdlet, and in this case the path is empty
+	"OutObfuscatedAssembly"
+	"Shuffle"
+	"Shuffle"
+	"UpdateCrmAssembly"
+	"UpdateCrmResouces"
+	"FindCrmUser"
+);
+
+$SourceFolders = @();
+
+foreach($Folder in $CmdletFolders)
+{
+  if ($Folder -ne "")
+  {
+    $Folder = "$Input$Folder\bin\Release"
+  }
+
+  $SourceFolders += $Folder
+}
+
+Write-Host
+
+Write-Host "Downloading latest VSTS SDK..."
+
+Save-Module -Name VstsTaskSdk -Path .
+
+Write-Host
+
+Write-Host "Copying required files to:"
+
+for ($i = 0; $i -lt $SourceFolders.Length; $i++)
+{
+  if ($SourceFolders[$i] -ne "")
+  {
+    Copy-Item -Path ($SourceFolders[$i]) -Filter "*.dll" -Recurse -Destination ($DestinationFolders[$i] + "\CI\") -Container
+  }
+
+  Copy-Item -Path ".\VstsTaskSdk" -Recurse -Destination $DestinationFolders[$i] -Container
+
+  Write-Host " * " $DestinationFolders[$i]
+}
+
+Remove-Item -Path ".\VstsTaskSdk" -Recurse -Force
+
+Write-Host
+Write-Host
+Write-Host
+
+Write-Host "Packing extension..."
+
+$Command = "tfx extension publish --manifest-globs .\vss-extension.json --output-path .\VSIX"
+
+if ((Read-Host -Prompt "Update revision? [Y/N]").ToLower() -eq "y")
+{
+  $Command += " --rev-version"
+}
+
+if ((Read-Host -Prompt "Publish extension? [Y/N]").ToLower() -eq "y")
+{
+  if(!(Test-Path -Path ".\pat.txt"))
+  {
+    $PAT = Read-Host -Prompt "Enter your PAT"
+    Add-Content ".\test.txt" $PAT
+  }
+  else
+  {
+    $PAT = Get-Content -Path ".\pat.txt"
+  }
+
+  $Command += " --token $PAT"
+}
+
+Invoke-Expression $Command
