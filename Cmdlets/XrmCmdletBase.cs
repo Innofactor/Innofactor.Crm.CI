597f640258ffcab9e7aade9a4fa36446688cf647
Imran Akram <imran.akram@innofactor.com>
2021-03-02 17:30:41 +0100

    Implementing connection retries by borrowing some more code from Wael Hamze Removed `OrganizationServiceProxy` Fix for #28

--- Cmdlets/XrmCmdletBase.cs
+++ Cmdlets/XrmCmdletBase.cs
@@ -4,21 +4,25 @@
 namespace Innofactor.Crm.CI.Cmdlets
 {
     using Microsoft.Xrm.Tooling.Connector;
+    using System;
     using System.Management.Automation;
     using System.Net;
+    using System.Threading;
 
     public abstract class XrmCmdletBase : Cmdlet
     {
         #region Private Fields
 
         private readonly int DefaultTime = 120;
+        private TimeSpan ConnectPolingInterval = TimeSpan.FromSeconds(15);
+        private int ConnectRetryCount = 3;
 
         #endregion Private Fields
 
         #region Public Properties
 
         /// <summary>
-        /// <para type="description">The connectionstring to the crm organization (see https://msdn.microsoft.com/en-us/library/mt608573.aspx ).</para>
+        /// <para type="description">The connectionstring to the crm organization (see https://msdn.microsoft.com/en-us/library/mt608573.aspx ). For Service Principal (app registration) based connections see https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-register-app</para>
         /// </summary>
         [Parameter(Mandatory = true)]
         public string ConnectionString
@@ -55,25 +59,38 @@ namespace Innofactor.Crm.CI.Cmdlets
         {
             base.BeginProcessing();
             SetSecurityProtocol();
-            WriteDebug("Connecting to CRM");
-            WriteVerbose($"Creating CrmServiceClient with: {ConnectionString}");
-            Service = new CrmServiceClient(ConnectionString);
-            if (Service == null)
+            WriteDebug("Connecting to CRM/Dataverse");
+            WriteVerbose($"Attempting to connect with: {ConnectionString}");
+
+            for (int i = 1; i <= ConnectRetryCount; i++)
             {
-                throw new PSArgumentException("Connection not established", "ConnectionString");
+                WriteVerbose($"Connecting to CRM/Dataverse [attempt {i}]");
+                Service = new CrmServiceClient(ConnectionString);
+
+                if (Service != null && Service.IsReady)
+                {
+                    if (Timeout == 0)
+                    {
+                        CrmServiceClient.MaxConnectionTimeout = new TimeSpan(0, 0, DefaultTime);
+                    }
+                    else
+                    {
+                        CrmServiceClient.MaxConnectionTimeout = new TimeSpan(0, 0, Timeout);
+                        WriteVerbose($"MaxConnectionTimeout set to {Timeout}");
+                    }
+
+                    return;
+                }
+                else
+                {
+                    if (i != ConnectRetryCount)
+                    {
+                        Thread.Sleep(ConnectPolingInterval);
+                    }
+                }
             }
-            if (Service.OrganizationServiceProxy == null)
-            {
-                throw new PSArgumentException($"Connection not established. Last CRM error message:\n{Service.LastCrmError}", "ConnectionString");
-            }
-            if (Timeout == 0)
-            {
-                Service.OrganizationServiceProxy.Timeout = new System.TimeSpan(0, 0, DefaultTime);
-            }
-            else
-            {
-                Service.OrganizationServiceProxy.Timeout = new System.TimeSpan(0, 0, Timeout);
-            }
+
+            throw new Exception($"Couldn't connect to CRM/Dataverse instance after {ConnectRetryCount} attempts: {Service?.LastCrmError}");
         }
 
         protected override void EndProcessing() =>
