e7ac2556e167211676ee9dacca104054730d3894
Imran Akram <imran@biznamics.com>
2024-01-15 11:17:37 +0100

    `UpdatePluginPackage` commandlet in the making

---
+++ Cmdlets/UpdatePluginPackage/UpdatePluginPackage.cs
@@ -0,0 +1,150 @@
+﻿using Innofactor.Crm.CI.Cmdlets;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Reflection;
+
+namespace UpdatePluginPackage
+{
+    [Cmdlet(VerbsData.Update, "PluginPackage")]
+    public class UpdatePluginPackage : XrmCmdletBase
+    {
+        #region Private Fields
+
+        private string fileculture;
+
+        private string filename;
+
+        private string filetoken;
+
+        private Version fileversion;
+
+        #endregion Private Fields
+
+        #region Public Properties
+
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            HelpMessage = "Path to assembly file"
+        ), Alias("DLL", "D")]
+        public string AssemblyFile { get; set; }
+
+        [Parameter(
+            HelpMessage = "Set this to allow updating managed assembly"
+        ), Alias("UM")]
+        public bool UpdateManaged { get; set; } = false;
+
+        #endregion Public Properties
+
+        #region Protected Methods
+
+        protected override void ProcessRecord()
+        {
+            var assembly = GetAssembly();
+            if (assembly != null)
+            {
+                UpdateAssembly(assembly);
+            }
+        }
+
+        #endregion Protected Methods
+
+        #region Private Methods
+
+        private Entity GetAssembly()
+        {
+            WriteObject($"Reading assembly file {AssemblyFile}");
+            var file = ReadFile(AssemblyFile);
+            WriteVerbose("Loading assembly file");
+            var assembly = Assembly.Load(file);
+            var chunks = assembly.FullName.Split(new string[] { ", ", "Version=", "Culture=", "PublicKeyToken=" }, StringSplitOptions.RemoveEmptyEntries);
+            filename = chunks[0];
+            fileversion = new Version(chunks[1]);
+            fileculture = chunks[2];
+            filetoken = chunks[3];
+            WriteObject($"Loaded assembly {filename} {fileversion}");
+            WriteVerbose($"Culture: {fileculture}");
+            WriteVerbose($"Token  : {filetoken}");
+
+            var query = new QueryExpression("pluginassembly");
+            query.ColumnSet.AddColumns("name", "version", "ismanaged");
+            query.Criteria.AddCondition("name", ConditionOperator.Equal, filename);
+            query.Criteria.AddCondition("version", ConditionOperator.Like, fileversion.ToString(2) + "%");
+            query.Criteria.AddCondition("culture", ConditionOperator.Equal, fileculture);
+            query.Criteria.AddCondition("publickeytoken", ConditionOperator.Equal, filetoken);
+
+            var plugin = Service.RetrieveMultiple(query).Entities.FirstOrDefault();
+
+            if (plugin != null)
+            {
+                WriteObject($"Found plugin: {plugin.Attributes["name"]} {plugin.Attributes["version"]}");
+                if (plugin.Attributes["ismanaged"] as bool? ?? false)
+                {
+                    if (!UpdateManaged)
+                    {
+                        throw new ArgumentOutOfRangeException("AssemblyFile", AssemblyFile, "Assembly is managed in target CRM. Use parameter UpdateManaged to allow this.");
+                    }
+                    else
+                    {
+                        WriteWarning("Updating managed assembly");
+                    }
+                }
+                return plugin;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("AssemblyFile", AssemblyFile, "Assembly does not appear to be registered in CRM");
+            }
+        }
+
+        private byte[] ReadFile(string fileName)
+        {
+            byte[] buffer = null;
+            using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                buffer = new byte[fs.Length];
+                fs.Read(buffer, 0, (int)fs.Length);
+            }
+            return buffer;
+        }
+
+        private void UpdateAssembly(Entity plugin)
+        {
+            try
+            {
+                WriteVerbose($"Reading assembly file {AssemblyFile}");
+                var file = ReadFile(AssemblyFile);
+                WriteVerbose("Adding Base64String to entity");
+                var updateplugin = plugin;
+                if (updateplugin.Attributes.Contains("version"))
+                {
+                    updateplugin.Attributes["version"] = fileversion.ToString();
+                }
+                else
+                {
+                    updateplugin.Attributes.Add("version", fileversion.ToString());
+                }
+                if (updateplugin.Attributes.Contains("content"))
+                {
+                    updateplugin.Attributes["content"] = Convert.ToBase64String(file);
+                }
+                else
+                {
+                    updateplugin.Attributes.Add("content", Convert.ToBase64String(file));
+                }
+                WriteObject("Saving updated assembly record");
+                Service.Update(updateplugin);
+            }
+            catch (Exception ex)
+            {
+                WriteError(new ErrorRecord(ex, "UpdateCrmAssembly", ErrorCategory.WriteError, plugin));
+            }
+        }
+
+        #endregion Private Methods
+    }
+}
